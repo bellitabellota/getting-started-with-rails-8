aed5ca4f23c81c9cdee04d5a3a06337f7776e3e7
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-10-30 15:12:33 +0000

    Add a (singular) resource route for sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
+  resource :sign_up
 
     resources :products do
     resources :subscribers, only: [ :create ]
